e865ed53425aa4f56fcf3d81111b0767bb7522f4
Tim Loden <tim.m.loden@gmail.com>
2020-04-07 08:30:43 -0700

    added bootstrap nav walker

--- functions.php
+++ functions.php
@@ -21,24 +21,15 @@ if (! function_exists('theme_setup') ) :
         // Add default posts and comments RSS feed links to head.
         add_theme_support('automatic-feed-links');
 
-        /*
-        * Enable support for Post Thumbnails on posts and pages.
-        *
-        * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-        */
         add_theme_support('post-thumbnails');
 
         // This theme uses wp_nav_menu() in one location.
         register_nav_menus(
             array(
-            'menu-1' => esc_html__('Primary', 'underscores'),
+            'header-primary' => esc_html__('Header Primary', 'restoration-performance'),
             ) 
         );
 
-        /*
-        * Switch default core markup for search form, comment form, and comments
-        * to output valid HTML5.
-        */
         add_theme_support(
             'html5', array(
             'search-form',
@@ -49,57 +40,17 @@ if (! function_exists('theme_setup') ) :
             ) 
         );
 
-        // Set up the WordPress core custom background feature.
-        add_theme_support(
-            'custom-background', apply_filters(
-                'underscores_custom_background_args', array(
-                'default-color' => 'ffffff',
-                'default-image' => '',
-                ) 
-            ) 
-        );
-
         // Add theme support for selective refresh for widgets.
         add_theme_support('customize-selective-refresh-widgets');
 
-        /**
-         * Add support for core custom logo.
-         *
-         * @link https://codex.wordpress.org/Theme_Logo
-         */
-        add_theme_support(
-            'custom-logo', array(
-            'height'      => 250,
-            'width'       => 250,
-            'flex-width'  => true,
-            'flex-height' => true,
-            ) 
-        );
     }
 endif;
 
 add_action('after_setup_theme', 'theme_setup');
 
 /**
- * Register widget area.
- *
- * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
+ * Load includes
  */
-function underscores_widgets_init()
-{
-    register_sidebar(
-        array(
-        'name'          => esc_html__('Sidebar', 'underscores'),
-        'id'            => 'sidebar-1',
-        'description'   => esc_html__('Add widgets here.', 'underscores'),
-        'before_widget' => '<section id="%1$s" class="widget %2$s">',
-        'after_widget'  => '</section>',
-        'before_title'  => '<h2 class="widget-title">',
-        'after_title'   => '</h2>',
-        ) 
-    );
-}
-add_action('widgets_init', 'underscores_widgets_init');
 
 function includes_autoload()
 {
@@ -111,3 +62,12 @@ function includes_autoload()
 }
 
 add_action('after_setup_theme', 'includes_autoload');
+
+/**
+ * Register Custom Navigation Walker
+ */
+function register_navwalker()
+{
+    include_once get_template_directory() . '/vendor/class-wp-bootstrap-navwalker.php';
+}
+add_action('after_setup_theme', 'register_navwalker');
--- header.php
+++ header.php
@@ -32,42 +32,27 @@
         </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Dropdown
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="#">Another action</a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">Something else here</a>
-                </div>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-            </li>
-            </ul>
+            <?php
+            wp_nav_menu(
+                array(
+                'theme_location'  => 'header-primary',
+                'depth'           => 2, // 1 = no dropdowns, 2 = with dropdowns.
+                'container'       => 'div',
+                'container_class' => 'collapse navbar-collapse',
+                'container_id'    => 'bs-example-navbar-collapse-1',
+                'menu_class'      => 'navbar-nav mr-auto',
+                'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
+                'walker'          => new WP_Bootstrap_Navwalker(),
+                ) 
+            );
+            ?>
             <form class="form-inline my-2 my-lg-0">
                 <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
                 <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
             </form>
         </div>
         </nav>
-        <?php
-        wp_nav_menu(
-            array(
-            'theme_location' => 'menu-1',
-            'menu_id'        => 'primary-menu',
-            ) 
-        );
-        ?>
+
                    
         </div>        
     </header><!-- #masthead -->
