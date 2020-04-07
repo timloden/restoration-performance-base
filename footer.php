885fa3fc7771a0681fea39be981187fe35cee17a
Tim Loden <tim.m.loden@gmail.com>
2020-04-07 07:26:30 -0700

    cleaned up header and footer

--- footer.php
+++ footer.php
@@ -13,21 +13,11 @@
 
 	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'underscores' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'underscores' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'underscores' ), 'underscores', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+	<footer class="site-footer">
+		<div class="container">
+			&copy; Classic Body Parts 2020
+		</div>
+	</footer>
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
