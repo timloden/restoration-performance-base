929a5482c0c1a7b3c980e86b38b9cc4652b5fa0d
Tim Loden <tim.m.loden@gmail.com>
2020-05-09 12:04:05 -0700

    added fitment to product detail page

--- woocommerce/single-product/meta.php
+++ woocommerce/single-product/meta.php
@@ -27,23 +27,45 @@ global $product;
 
     <?php if (wc_product_sku_enabled() && ( $product->get_sku() || $product->is_type('variable') ) ) : ?>
 
-    <div class="sku_wrapper d-block"><?php esc_html_e('SKU:', 'woocommerce'); ?> <span
+    <p class="sku_wrapper mb-1"><strong><?php esc_html_e('SKU:', 'woocommerce'); ?></strong> <span
             class="sku"><?php echo ( $sku = $product->get_sku() ) ? $sku : esc_html__('N/A', 'woocommerce'); ?></span>
-    </div>
+    </p>
 
     <?php endif; ?>
 
-    <?php echo wc_get_product_category_list($product->get_id(), ', ', '<div class="posted_in d-block">' . _n('Category:', 'Categories:', count($product->get_category_ids()), 'woocommerce') . ' ', '</div>'); ?>
+    <?php echo wc_get_product_category_list($product->get_id(), ', ', '<p class="posted_in mb-1">' . _n('<strong>Category:</strong>', '<strong>Categories:</strong>', count($product->get_category_ids()), 'woocommerce') . ' ', '</p>'); ?>
 
-    <?php echo wc_get_product_tag_list( $product->get_id(), ', ', '<div class="tagged_as">' . _n( 'Tag:', 'Tags:', count( $product->get_tag_ids() ), 'woocommerce' ) . ' ', '</div>' ); ?>
+    <?php echo wc_get_product_tag_list( $product->get_id(), ', ', '<p class="tagged_as mb-1">' . _n( '<strong>Tag:</strong>', '<strong>Tags:</strong>', count( $product->get_tag_ids() ), 'woocommerce' ) . ' ', '</p>' ); ?>
 
     <?php
     $shipping_class = $product->get_shipping_class();
     if (strpos($shipping_class, '-freight')) {
-        echo '<div class="d-block py-3"><i class="las la-shipping-fast"></i> Freight Item</div>';
+        echo '<p class="mb-1"><i class="las la-shipping-fast"></i> Freight Item</p>';
     }
     ?>
-    <div class="d-block">Brand: <?php echo get_brand_name($product->get_id()); ?></div>
+    <p class="mb-1"><strong>Brand:</strong> <?php echo get_brand_name($product->get_id()); ?></p>
+
+    <?php if( have_rows('vehicle_fitment') ): ?>
+    <p class="mb-1"><strong>Fitment:</strong></p>
+    <ul>
+
+        <?php while( have_rows('vehicle_fitment') ): the_row(); 
+
+            // vars
+            $vehicle = get_sub_field('vehicle');
+
+
+            ?>
+
+        <li>
+            <?php echo $vehicle; ?>
+        </li>
+
+        <?php endwhile; ?>
+
+    </ul>
+
+    <?php endif; ?>
 
     <?php do_action('woocommerce_product_meta_end'); ?>
 </div>
