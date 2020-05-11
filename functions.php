36c3576d95f17299ffd11a9160b4355c958637e7
Tim Loden <tim.m.loden@gmail.com>
2020-05-11 15:58:56 -0700

    added header and footer embeds

--- functions.php
+++ functions.php
@@ -76,6 +76,19 @@ endif;
 
 add_action('after_setup_theme', 'theme_setup');
 
+if( function_exists('acf_add_options_page') ) {
+	
+	acf_add_options_page(array(
+		'page_title' 	=> 'Site Options',
+		'menu_title'	=> 'Site Options',
+		'menu_slug' 	=> 'site-options',
+		'capability'	=> 'edit_posts',
+		'redirect'		=> false
+	));
+	
+	
+}
+
 /**
  * Load includes
  */
