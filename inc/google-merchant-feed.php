44e94b465ec18cef04d6337fc81fb438b95099f5
Tim Loden <tim.m.loden@gmail.com>
2020-06-12 10:46:40 -0700

    added fitment to description in google feed

--- inc/google-merchant-feed.php
+++ inc/google-merchant-feed.php
@@ -12,8 +12,22 @@ add_filter( 'woocommerce_gpf_shipping_weight_unit', function ( $unit ) {
 function lw_woocommerce_gpf_feed_item_google( $feed_item, $product ) {
 	// Modify $feed_item->description here.
     //$feed_item->description = 'test';
+    $product_id = $product->get_id();
+    $description = $feed_item->description;
+    $list = '';
     
-    
+    if( have_rows('vehicle_fitment', $product_id) ):
+        $list = '<p>Vehicle Fitment:</p><ul>';
+        while ( have_rows('vehicle_fitment', $product_id) ) : the_row();
+            $vehicle = '<li>' . get_sub_field('vehicle') . '</li>';
+            $list .= $vehicle;
+        endwhile;
+        $list .= '</ul>';
+    else :
+        // no rows found
+    endif;
+
+    $feed_item->description = $description . $list;
     
     return $feed_item;
 }
