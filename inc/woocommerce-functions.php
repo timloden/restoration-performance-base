a9eac8c44d9572054b08c8f309cb3f695f0866de
Tim Loden <tim.m.loden@gmail.com>
2020-07-01 11:19:44 -0700

    added comment to removing freight

--- inc/woocommerce-functions.php
+++ inc/woocommerce-functions.php
@@ -134,11 +134,13 @@ add_filter('woocommerce_package_rates', 'custom_shipping_option', 20, 2 );
 
 function custom_shipping_option($rates){
 
-    // Only unset rates if $4.50 shipping is available
+    // unset rates if $4.50 shipping is available
 
     if ( isset( $rates['flexible_shipping_1_2'] ) ) {
         unset( $rates['flexible_shipping_fedex:0:GROUND_HOME_DELIVERY'] );
-    }     
+    }  
+    
+    // if freight or heavy-freight, remove fedex fallback
 
     if ( isset( $rates['flexible_shipping_1_1'] ) || isset( $rates['flexible_shipping_1_4'] ) ) {
         unset( $rates['flexible_shipping_fedex:fallback'] );
