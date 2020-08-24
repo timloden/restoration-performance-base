e598b8a2403aafe30d57f575e09662da860db7ab
Tim Loden <tim.m.loden@gmail.com>
2020-08-24 14:45:11 -0700

    Updated footer form toe use GF

--- footer.php
+++ footer.php
@@ -82,43 +82,9 @@
                     trademark Chevy, Chevrolet, Ford and Mopar.</p>
                 <p class="mb-2 font-weight-bold">Get all the latest product updates, specials and coupons!</p>
 
-                <div class="mb-3" id="mc_embed_signup">
-                    <form
-                        action="https://classicbodyparts.us8.list-manage.com/subscribe/post?u=7f3a72c126eb4512af40c4195&amp;id=7af00b4ad6"
-                        method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate"
-                        target="_blank" novalidate>
-                        <label for="mce-EMAIL">Email Address <span class="asterisk">*</span>
-                        </label>
-                        <div class="input-group">
-
-                            <input type="email" value="" name="EMAIL" class="required email form-control"
-                                id="mce-EMAIL">
-                            <div class="input-group-append">
-                                <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe"
-                                    class="button btn btn-secondary">
-                            </div>
-                        </div>
-                        <div id="mce-responses" class="clear">
-                            <div class="response" id="mce-error-response" style="display:none"></div>
-                            <div class="response" id="mce-success-response" style="display:none"></div>
-                        </div>
-                        <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-                        <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text"
-                                name="b_7f3a72c126eb4512af40c4195_7af00b4ad6" tabindex="-1" value=""></div>
-                    </form>
+                <div class="mb-3">
+                    <?php gravity_form( 2, false, false, false, '', true, 12 ); ?>
                 </div>
-                <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'>
-                </script>
-                <script type='text/javascript'>
-                (function($) {
-                    window.fnames = new Array();
-                    window.ftypes = new Array();
-                    fnames[0] = 'EMAIL';
-                    ftypes[0] = 'email';
-                }(jQuery));
-                var $mcj = jQuery.noConflict(true);
-                </script>
-                <!--End mc_embed_signup-->
 
             </div>
         </div>
