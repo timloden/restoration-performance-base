331b5143f47981d8558cdfbe1acd6070a3abcb52
Tim Loden <tim.m.loden@gmail.com>
2020-12-27 17:28:00 -0800

    updated product bundle templates to latest version

--- woocommerce/single-product/bundled-item-quantity.php
+++ woocommerce/single-product/bundled-item-quantity.php
@@ -8,7 +8,7 @@
  * We try to do this as little as possible, but it does happen.
  * When this occurs the version of the template file will be bumped and the readme will list any important changes.
  *
- * @version 5.8.0
+ * @version 6.4.0
  */
 
 // Exit if accessed directly.
@@ -19,13 +19,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 if ( $hide_input ) {
 
 	?><div class="quantity <?php echo 'tabular' !== $layout ? 'quantity_hidden' : ''; ?>">
-		<input class="qty bundled_qty" type="hidden" name="<?php echo $input_name; ?>" value="<?php echo $quantity_min; ?>" /><?php
+    <input class="qty bundled_qty" type="hidden" name="<?php echo $input_name; ?>"
+        value="<?php echo $quantity_min; ?>" /><?php
 
 		if ( 'tabular' === $layout && ( $quantity_min > 0 || $bundled_item->is_in_stock() ) ) {
 			echo $quantity_min;
 		}
 
-	?></div><?php
+	?>
+</div><?php
 
 } else {
 
@@ -35,7 +37,7 @@ if ( $hide_input ) {
 		'input_name'  => $input_name,
 		'min_value'   => $quantity_min,
 		'max_value'   => $quantity_max,
-		'input_value' => isset( $_REQUEST[ $input_name ] ) ? absint( $_REQUEST[ $input_name ] ) : apply_filters( 'woocommerce_bundled_product_quantity', $quantity_min, $quantity_min, $quantity_max, $bundled_item )
+		'input_value' => isset( $_REQUEST[ $input_name ] ) ? absint( $_REQUEST[ $input_name ] ) : apply_filters( 'woocommerce_bundled_product_quantity', $quantity_default, $quantity_min, $quantity_max, $bundled_item )
 	), $bundled_item->product );
 
 	echo preg_replace( '/(class=\"[^\"]*qty)([\"\ ])/', '$1 bundled_qty$2', ob_get_clean() );
