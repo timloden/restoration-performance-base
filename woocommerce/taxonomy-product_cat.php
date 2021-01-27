7b58616ffc8a1849f1eecb1590e896418b14dcf6
Tim Loden <tim.m.loden@gmail.com>
2021-01-27 11:27:21 -0800

    woocommerce 4.9.2 template updates

--- woocommerce/taxonomy-product_cat.php
+++ woocommerce/taxonomy-product_cat.php
@@ -2,7 +2,7 @@
 /**
  * The Template for displaying products in a product category. Simply includes the archive template
  *
- * This template can be overridden by copying it to yourtheme/woocommerce/taxonomy-product_cat.php.
+ * This template can be overridden by copying it to yourtheme/woocommerce/taxonomy-product-cat.php.
  *
  * HOWEVER, on occasion WooCommerce will need to update template files and you
  * (the theme developer) will need to copy the new files to your theme to
@@ -12,7 +12,7 @@
  *
  * @see         https://docs.woocommerce.com/document/template-structure/
  * @package     WooCommerce/Templates
- * @version     1.6.4
+ * @version     4.7.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
