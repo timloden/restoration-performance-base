2e100d4e52e5eeb9bd4737effbc3ed8ceed0e462
Tim Loden <tim.m.loden@gmail.com>
2021-08-19 09:45:16 -0700

    updated email templates for v5.6

--- woocommerce/emails/plain/email-addresses.php
+++ woocommerce/emails/plain/email-addresses.php
@@ -11,28 +11,30 @@
  * the readme will list any important changes.
  *
  * @see     https://docs.woocommerce.com/document/template-structure/
- * @package WooCommerce/Templates/Emails/Plain
- * @version 3.4.0
+ * @package WooCommerce\Templates\Emails\Plain
+ * @version 5.6.0
  */
 
 defined( 'ABSPATH' ) || exit;
 
 echo "\n" . esc_html( wc_strtoupper( esc_html__( 'Billing address', 'woocommerce' ) ) ) . "\n\n";
-echo preg_replace( '#<br\s*/?>#i', "\n", $order->get_formatted_billing_address() ) . "\n"; // WPCS: XSS ok.
-
+echo preg_replace( '#<br\s*/?>#i', "\n", $order->get_formatted_billing_address() ) . "\n";
 if ( $order->get_billing_phone() ) {
-	echo $order->get_billing_phone() . "\n"; // WPCS: XSS ok.
+echo $order->get_billing_phone() . "\n"; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 }
 
 if ( $order->get_billing_email() ) {
-	echo $order->get_billing_email() . "\n"; // WPCS: XSS ok.
+echo $order->get_billing_email() . "\n"; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 }
 
 if ( ! wc_ship_to_billing_address_only() && $order->needs_shipping_address() ) {
-	$shipping = $order->get_formatted_shipping_address();
+$shipping = $order->get_formatted_shipping_address();
 
-	if ( $shipping ) {
-		echo "\n" . esc_html( wc_strtoupper( esc_html__( 'Shipping address', 'woocommerce' ) ) ) . "\n\n";
-		echo preg_replace( '#<br\s*/?>#i', "\n", $shipping ) . "\n"; // WPCS: XSS ok.
-	}
-}
+if ( $shipping ) {
+echo "\n" . esc_html( wc_strtoupper( esc_html__( 'Shipping address', 'woocommerce' ) ) ) . "\n\n";
+echo preg_replace( '#<br\s* /?>#i', "\n", $shipping ) . "\n";
+    if ( $order->get_shipping_phone() ) {
+    echo $order->get_shipping_phone() . "\n"; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+    }
+    }
+    }
