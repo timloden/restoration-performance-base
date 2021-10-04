72e08e03b78f44cb5b0263ad39fcda6e1e4117e6
Tim Loden <tim.m.loden@gmail.com>
2021-10-04 14:54:51 -0700

    moved store name in admin title

--- inc/admin.php
+++ inc/admin.php
@@ -33,4 +33,11 @@ function orders_admin_bar_form() {
         ));
     } 
   }
-  add_action('admin_bar_menu', 'orders_admin_bar_form');
+add_action('admin_bar_menu', 'orders_admin_bar_form');
+
+
+add_filter('admin_title', 'my_admin_title', 10, 2);
+
+function my_admin_title($admin_title, $title) {
+    return get_bloginfo('name').' | '.$title;
+}
