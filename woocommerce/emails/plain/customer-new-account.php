760fcd1688705afa439e4594ac40c3cbd4693673
Tim Loden <tim.m.loden@gmail.com>
2022-01-28 16:41:44 -0800

    fixed plain new account email

--- woocommerce/emails/plain/customer-new-account.php
+++ woocommerce/emails/plain/customer-new-account.php
@@ -2,7 +2,7 @@
 /**
  * Customer new account email
  *
- * This template can be overridden by copying it to yourtheme/woocommerce/emails/customer-new-account.php.
+ * This template can be overridden by copying it to yourtheme/woocommerce/emails/plain/customer-new-account.php.
  *
  * HOWEVER, on occasion WooCommerce will need to update template files and you
  * (the theme developer) will need to copy the new files to your theme to
@@ -11,32 +11,36 @@
  * the readme will list any important changes.
  *
  * @see https://docs.woocommerce.com/document/template-structure/
- * @package WooCommerce\Templates\Emails
+ * @package WooCommerce\Templates\Emails\Plain
  * @version 6.0.0
  */
 
 defined( 'ABSPATH' ) || exit;
 
-do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
+echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n";
+echo esc_html( wp_strip_all_tags( $email_heading ) );
+echo "\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n";
 
-<?php /* translators: %s: Customer username */ ?>
-<p><?php printf( esc_html__( 'Hi %s,', 'woocommerce' ), esc_html( $user_login ) ); ?></p>
-<?php /* translators: %1$s: Site title, %2$s: Username, %3$s: My account link */ ?>
-<p><?php printf( esc_html__( 'Thanks for creating an account on %1$s. Your username is %2$s. You can access your account area to view orders, change your password, and more at: %3$s', 'woocommerce' ), esc_html( $blogname ), '<strong>' . esc_html( $user_login ) . '</strong>', make_clickable( esc_url( wc_get_page_permalink( 'myaccount' ) ) ) ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
-</p>
-<?php if ( 'yes' === get_option( 'woocommerce_registration_generate_password' ) && $password_generated && $set_password_url ) : ?>
-<?php // If the password has not been set by the user during the sign up process, send them a link to set a new password ?>
-<p><a
-        href="<?php echo esc_attr( $set_password_url ); ?>"><?php printf( esc_html__( 'Click here to set your new password.', 'woocommerce' ) ); ?></a>
-</p>
-<?php endif; ?>
+/* translators: %s: Customer username */
+echo sprintf( esc_html__( 'Hi %s,', 'woocommerce' ), esc_html( $user_login ) ) . "\n\n";
+/* translators: %1$s: Site title, %2$s: Username, %3$s: My account link */
+echo sprintf( esc_html__( 'Thanks for creating an account on %1$s. Your username is %2$s. You can access your account area to view orders, change your password, and more at: %3$s', 'woocommerce' ), esc_html( $blogname ), esc_html( $user_login ), esc_html( wc_get_page_permalink( 'myaccount' ) ) ) . "\n\n";
 
-<?php
+// Only send the set new password link if the user hasn't set their password during sign-up.
+if ( 'yes' === get_option( 'woocommerce_registration_generate_password' ) && $password_generated && $set_password_url ) {
+	/* translators: URL follows */
+	echo esc_html__( 'To set your password, visit the following address: ', 'woocommerce' ) . "\n\n";
+	echo esc_html( $set_password_url ) . "\n\n";
+}
+
+echo "\n\n----------------------------------------\n\n";
+
 /**
  * Show user-defined additional content - this is set in each email's settings.
  */
 if ( $additional_content ) {
-	echo wp_kses_post( wpautop( wptexturize( $additional_content ) ) );
+	echo esc_html( wp_strip_all_tags( wptexturize( $additional_content ) ) );
+	echo "\n\n----------------------------------------\n\n";
 }
 
-do_action( 'woocommerce_email_footer', $email );
+echo wp_kses_post( apply_filters( 'woocommerce_email_footer_text', get_option( 'woocommerce_email_footer_text' ) ) );
