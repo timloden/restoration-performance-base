03ecbb109e1d7cbdfbd389e6c003e176439ceb33
Tim Loden <tim.m.loden@gmail.com>
2022-02-08 16:44:30 -0800

    updated dynacorn import to need 2 in each warehouse to be in stock

--- inc/wp-all-import-functions.php
+++ inc/wp-all-import-functions.php
@@ -35,7 +35,7 @@ function dynacorn_pricing( $cost = null, $model = '' ) {
 
 function dynacorn_stock_status( $ca = null, $pa = null ) {
 
-    if ($ca > 1 || $pa > 1) {
+    if ($ca >= 1 && $pa >= 1) {
         $stock = 'instock';
     } else {
         $stock = 'onbackorder';
