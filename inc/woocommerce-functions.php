e4365fb58cfcbb3881f92b9e4350abcbceec4345
Tim Loden <tim.m.loden@gmail.com>
2022-02-23 08:37:06 -0800

    temp commented out state filter

--- inc/woocommerce-functions.php
+++ inc/woocommerce-functions.php
@@ -290,15 +290,15 @@ function order_address_classification( $order ){
 
 }
 
-add_filter( 'woocommerce_states', 'custom_us_states', 10, 1 );
+// add_filter( 'woocommerce_states', 'custom_us_states', 10, 1 );
 
-function custom_us_states( $states ) {
-    $non_allowed_us_states = array( 'AK', 'HI', 'AA', 'AE', 'AP'); 
+// function custom_us_states( $states ) {
+//     $non_allowed_us_states = array( 'AK', 'HI', 'AA', 'AE', 'AP'); 
 
-    // Loop through your non allowed us states and remove them
-    foreach( $non_allowed_us_states as $state_code ) {
-        if( isset($states['US'][$state_code]) )
-            unset( $states['US'][$state_code] );
-    }
-    return $states;
-}
+//     // Loop through your non allowed us states and remove them
+//     foreach( $non_allowed_us_states as $state_code ) {
+//         if( isset($states['US'][$state_code]) )
+//             unset( $states['US'][$state_code] );
+//     }
+//     return $states;
+// }
