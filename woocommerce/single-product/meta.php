24000f5e5a0dd359f8891c1f744236e3abd43dc5
Tim Loden <tim.m.loden@gmail.com>
2022-06-21 23:06:57 -0700

    reformatted product detail meta

--- woocommerce/single-product/meta.php
+++ woocommerce/single-product/meta.php
@@ -20,36 +20,10 @@ if (! defined('ABSPATH') ) {
 }
 
 global $product;
-$product_id = $product->get_id();
-
-$shipping_class_id   = $product->get_shipping_class_id();
-$shipping_class_term = get_term($shipping_class_id, 'product_shipping_class');
-$shipping_class = $shipping_class_term->slug;
-$stock_status = $product->get_stock_status();
 $upsells = $product->get_upsells();
 
 ?>
 <div class="product_meta">
-    <?php 
-    $brand = wp_get_object_terms( $product_id, 'pwb-brand' );
-    if($brand) {
-        $term = 'term_' . $brand[0]->term_id;
-        
-        if (strpos($shipping_class, 'ground') !== false && get_field('ground_notice', $term) && $stock_status == 'instock') {
-            echo '<div class="alert alert-info">';
-            echo get_field('ground_notice', $term);
-            echo '</div>';
-        }
-        
-        if (strpos($shipping_class, '-freight') !== false && get_field('freight_notice', $term) && $stock_status == 'instock') {
-            echo '<div class="alert alert-info">';
-            echo get_field('freight_notice', $term);
-            echo '</div>';
-        }
-        
-    }
-    ?>
-
     <?php do_action('woocommerce_product_meta_start'); ?>
 
     <?php //echo wc_get_product_category_list($product_id, ', ', '<p class="posted_in mb-3">' . _n('', '', count($product->get_category_ids()), 'woocommerce') . ' ', '</p>'); ?>
