2be9fbe43e0f483ea13752bd25a8c596f7be16cd
Tim Loden <tim.m.loden@gmail.com>
2022-11-14 17:08:38 -0800

    formatting

--- inc/searchwp.php
+++ inc/searchwp.php
@@ -55,13 +55,16 @@ add_filter( 'searchwp\query\mods', function( $mods ) {
 	$goodmark_term = get_term_by('name', 'Goodmark', 'pwb-brand');
 	
 	// Taxonomy bonus weight Mods.
-	$bonuses = [ [
-		'term_id' => $dii_term->term_id,  // Term ID to receive extra weight.
-		'weight'  => 200, // How much extra weight for this term.
-	], [
-		'term_id' => $goodmark_term->term_id,  // Term ID to receive extra weight.
-		'weight'  => 200, // How much extra weight for this term.
-	],  ];
+	$bonuses = [ 
+		[
+			'term_id' => $dii_term->term_id,  // Term ID to receive extra weight.
+			'weight'  => 200, // How much extra weight for this term.
+		], 
+		[
+			'term_id' => $goodmark_term->term_id,  // Term ID to receive extra weight.
+			'weight'  => 200, // How much extra weight for this term.
+		],  
+	];
 
 	$term_mods = [];
 
