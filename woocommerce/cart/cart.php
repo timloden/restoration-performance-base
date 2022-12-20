def6b88f4f14e4cc0bdcf08a9d8029a0e3568265
Tim Loden <tim.m.loden@gmail.com>
2022-12-20 14:10:00 -0800

    updated cart template file to 7.0.1

--- woocommerce/cart/cart.php
+++ woocommerce/cart/cart.php
@@ -13,7 +13,7 @@
  *
  * @see     https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates
- * @version 4.4.0
+ * @version 7.0.1
  */
 
 defined('ABSPATH') || exit;
@@ -108,14 +108,18 @@ $all_shipping_classes = [];
                                 data-title="<?php esc_attr_e('Quantity', 'woocommerce'); ?>">
                                 <?php
                             if ($_product->is_sold_individually()) {
+                                $min_quantity = 1;
+							    $max_quantity = 1;
                                 $product_quantity = sprintf('1 <input type="hidden" name="cart[%s][qty]" value="1" />', $cart_item_key);
                             } else {
+                                $min_quantity = 0;
+							    $max_quantity = $_product->get_max_purchase_quantity();
                                 $product_quantity = woocommerce_quantity_input(
                                     array(
                                         'input_name'   => "cart[{$cart_item_key}][qty]",
                                         'input_value'  => $cart_item['quantity'],
-                                        'max_value'    => $_product->get_max_purchase_quantity(),
-                                        'min_value'    => '0',
+                                        'max_value'    => $max_quantity,
+                                        'min_value'    => $min_quantity,
                                         'product_name' => $_product->get_name(),
                                     ),
                                     $_product,
