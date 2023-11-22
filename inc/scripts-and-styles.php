6493cc7423cdd10efffed5477cca283070551e9d
Tim Loden <tim.m.loden@gmail.com>
2023-11-22 11:49:12 -0800

    added versioning to style file

--- inc/scripts-and-styles.php
+++ inc/scripts-and-styles.php
@@ -5,7 +5,7 @@
  */
 function scripts_and_styles()
 {
-    wp_enqueue_style('base-style', get_stylesheet_uri());
+    wp_enqueue_style('base-style', get_template_directory_uri() . '/style.css', array(), '1.21.0');
 
     wp_enqueue_script('vendor-js', get_template_directory_uri() . '/assets/js/vendor.min.js', array(), '', true);
     wp_enqueue_script('custom-js', get_template_directory_uri() . '/assets/js/custom.min.js', array(), '', true);
