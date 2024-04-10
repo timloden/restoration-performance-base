c1ed1a0ba096c17879b33cfc5d792d88c77c8d7b
Tim Loden <tim.m.loden@gmail.com>
2024-04-10 16:42:39 -0700

    form-edit-account to 8.7.0

--- woocommerce/myaccount/form-edit-account.php
+++ woocommerce/myaccount/form-edit-account.php
@@ -12,7 +12,7 @@
  *
  * @see https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce/Templates
- * @version 7.0.1
+ * @version 8.7.0
  */
 
 defined( 'ABSPATH' ) || exit;
@@ -59,6 +59,15 @@ do_action( 'woocommerce_before_edit_account_form' ); ?>
             id="account_email" autocomplete="email" value="<?php echo esc_attr( $user->user_email ); ?>" />
     </div>
 
+    <?php
+		/**
+		 * Hook where additional fields should be rendered.
+		 *
+		 * @since 8.7.0
+		 */
+		do_action( 'woocommerce_edit_account_form_fields' );
+	?>
+
 
     <p class="fw-bold mb-1 mt-2"><?php esc_html_e( 'Password change', 'woocommerce' ); ?></p>
 
@@ -82,7 +91,14 @@ do_action( 'woocommerce_before_edit_account_form' ); ?>
 
 
 
-    <?php do_action( 'woocommerce_edit_account_form' ); ?>
+    <?php
+		/**
+		 * My Account edit account form.
+		 *
+		 * @since 2.6.0
+		 */
+		do_action( 'woocommerce_edit_account_form' );
+	?>
 
     <p>
         <?php wp_nonce_field( 'save_account_details', 'save-account-details-nonce' ); ?>
